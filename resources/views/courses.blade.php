22d9916085b10726bce1f3910e879dabb600642d
Mukesh Kumar <mukeshvindra@gmail.com>
2024-05-28 09:46:03 +0530

    first commit

---
+++ resources/views/courses.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+@push('title')
+<title>courses</title>
+@endpush
+@section('main-section')
+<h1>courses Page</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\blogController;
+use App\Http\Controllers\SigleActionController;
+use App\Http\Controllers\photoController;
+
+Route::get('/', [blogController::class,'index']);
+Route::get('/about','App\Http\Controllers\blogController@about');
+Route::get('/courses', SigleActionController::class);
+Route::resource('/photo',photoController::class);
